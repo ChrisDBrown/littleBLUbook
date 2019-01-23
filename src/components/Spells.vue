d6b2e6041b7cf81b5ffb2b31643cda52d0de6324
Chris D Brown <owner@chrisdbrown.co.uk>
2019-01-23 23:14:46 +0000

    feat: Add all location info

--- src/components/Spells.vue
+++ src/components/Spells.vue
@@ -1,53 +1,76 @@
 <template>
-  <el-table
-    :data="spells">
-    <el-table-column
-      type="selection">
-    </el-table-column>
-    <el-table-column
-      prop="id"
-      label="No."
-      sortable>
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="Spell"
-      sortable>
-      <template slot-scope="scope">
-        <img :src="'/spells/' + scope.row.id + '.png'" width="55" /> {{ scope.row.name }}
-      </template>
-    </el-table-column>
-        <el-table-column
-      prop="rank"
-      label="Rank"
-      sortable>
-      <template slot-scope="scope">
-        <span v-for="n in scope.row.rank" :key="n">★</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="type"
-      label="Type"
-      sortable>
-    </el-table-column>
-    <el-table-column
-      prop="level"
-      label="Level"
-      sortable>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table
+      ref="spellsTable"
+      :data="spells"
+      v-on:row-click="handleRowClick">
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          {{ scope.row.locations }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="id"
+        label="No."
+        width="70"
+        sortable>
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="Spell"
+        sortable>
+        <template slot-scope="scope">
+          <div class="spell-name">
+            <img :src="'/spells/' + scope.row.id + '.png'" class="spell-thumb hidden-xs-only" /> <span>{{ scope.row.name }}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="rank"
+        label="Rank"
+        sortable>
+        <template slot-scope="scope">
+          <span v-for="n in scope.row.rank" :key="n">★</span>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog title="Map" :visible.sync="showMapBox">
+      {{ mapBoxInfo }}
+    </el-dialog>
+  </div>
 </template>
 
 <script>
   // Adapted from https://ffxiv.consolegameswiki.com/wiki/Blue_Magic_Spellbook
-  // TODO: Handle cases where there's multiple ways to get a spell
   const spells = require('../assets/spells.json')
 
   export default {
     data() {
       return {
-        spells: spells
+        spells: spells,
+        showMapBox: false,
+        mapBoxInfo: null
+      }
+    },
+    methods: {
+      handleRowClick(row) {
+        this.$refs.spellsTable.toggleRowExpansion(row);
       }
     }
   }
-  </script>
+</script>
+
+<style>
+.spell-thumb {
+  width: 55px;
+  border-radius: 20%;
+  padding-right: 5px;
+}
+
+.spell-name {
+  display:flex;
+  flex-wrap: nowrap;
+  align-items: center
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
-import { Container, Header, Main, Table, TableColumn } from 'element-ui'
+import { Button, Container, Dialog, Header, Main, Table, TableColumn } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css';
 
 locale.use(lang)
 
+Vue.use(Button)
 Vue.use(Container)
+Vue.use(Dialog)
 Vue.use(Header)
 Vue.use(Main)
 Vue.use(Table)
