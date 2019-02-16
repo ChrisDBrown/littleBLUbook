02e2e149a28670beda90c2cf6b52e09cc3be749a
Chris D Brown <owner@chrisdbrown.co.uk>
2019-02-16 18:09:31 +0000

    fix: Resolve mobile usability issues

--- src/components/Spells.vue
+++ src/components/Spells.vue
@@ -1,55 +1,79 @@
 <template>
-  <div>
-    <el-table
-      ref="spellsTable"
-      :data="spells"
-      v-on:row-click="handleRowClick">
+  <el-row
+    type="flex"
+    justify="center">
+    <el-col
+      :xs="24"
+      :sm="24"
+      :md="20"
+      :lg="16"
+      :xl="14">
+      <el-table
+        ref="spellsTable"
+        :data="spells"
+        v-on:row-click="handleRowClick">
 
-      <el-table-column type="expand">
-        <template slot-scope="scope">
-          <table>
-            <tr v-for="(location, index) in scope.row.locations" v-bind:key="index">
-              <Location :location="location" />
-            </tr>
-          </table>
-        </template>
-      </el-table-column>
+        <el-table-column
+          type="expand"
+          width="20">
+          <template slot-scope="scope">
+            <ul class="location-list">
+              <div v-for="(location, index) in scope.row.locations" v-bind:key="index">
+                <Location :location="location" />
+              </div>
+            </ul>
+          </template>
+        </el-table-column>
 
-      <el-table-column
-        prop="id"
-        label="No."
-        width="70"
-        sortable>
-      </el-table-column>
+        <el-table-column
+          prop="id"
+          label="No"
+          width="70"
+          header-align="center"
+          align="center"
+          sortable>
+        </el-table-column>
 
-      <el-table-column
-        prop="name"
-        label="Spell"
-        sortable>
-        <template slot-scope="scope">
-          <div class="spell-name">
-            <img :src="'/spells/' + scope.row.id + '.png'" class="spell-thumb hidden-xs-only" /> <span>{{ scope.row.name }}</span>
-          </div>
-        </template>
-      </el-table-column>
+        <el-table-column
+          prop="name"
+          label="Spell Name"
+          header-align="center"
+          sortable>
+          <template slot-scope="scope">
+            <div class="spell-name">
+              <img :src="'/spells/' + scope.row.id + '.png'" class="spell-thumb" /> <span>{{ scope.row.name }}</span>
+            </div>
+          </template>
+        </el-table-column>
 
-      <el-table-column
-        prop="rank"
-        label="Rank"
-        v-if="window.width > 600"
-        sortable>
-        <template slot-scope="scope">
-          <span v-for="n in scope.row.rank" :key="n">★</span>
-        </template>
-      </el-table-column>
+        <el-table-column
+          prop="rank"
+          label="Rank"
+          header-align="center"
+          align="center"
+          v-if="window.width > 600"
+          sortable>
+          <template slot-scope="scope">
+            <span v-for="n in scope.row.rank" :key="n">★</span>
+          </template>
+        </el-table-column>
 
-      <el-table-column>
-        <template slot-scope="scope">
-          <el-checkbox-button :value="isSpellLearned(scope.row.id)" v-on:change="handleLearnedClick(scope.row.id)">Learned</el-checkbox-button>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+        <el-table-column
+          width="80"
+          header-align="center"
+          :label="learnedOutOfTotal">
+          <template slot-scope="scope">
+            <el-checkbox-button
+              :value="isSpellLearned(scope.row.id)"
+              v-on:change="handleLearnedClick(scope.row.id)"
+              class="rounded-check">
+                Got
+              </el-checkbox-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -82,6 +106,11 @@
         this.window.height = window.innerHeight;
       }
     },
+    computed: {
+      learnedOutOfTotal() {
+        return this.$store.state.learned.length + '/' + this.spells.length
+      }
+    },
     created() {
       window.addEventListener('resize', this.handleResize)
       this.handleResize();
@@ -100,9 +129,27 @@
   padding-right: 5px;
 }
 
+@media only screen and (max-width: 600px) {
+  .spell-thumb {
+    width: 40px;
+  }
+}
+
 .spell-name {
   display:flex;
   flex-wrap: nowrap;
-  align-items: center
+  align-items: center;
+}
+
+.rounded-check .el-checkbox-button__inner {
+  border-radius: 20% !important;
+}
+
+td.el-table__expanded-cell[class*=cell] {
+  padding: 0;
+}
+
+.location-list li {
+  padding: 10px 0;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, CheckboxButton, Container, Dialog, Footer, Header, Main, Table, TableColumn } from 'element-ui'
+import { Button, CheckboxButton, Col, Container, Dialog, Footer, Header, Main, Row, Table, TableColumn } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -9,10 +9,12 @@ locale.use(lang)
 
 Vue.use(Button)
 Vue.use(CheckboxButton)
+Vue.use(Col)
 Vue.use(Container)
 Vue.use(Dialog)
 Vue.use(Footer)
 Vue.use(Header)
 Vue.use(Main)
+Vue.use(Row)
 Vue.use(Table)
 Vue.use(TableColumn)
