6562b97ce8c8d7715c3fabbd9aef52263b985b74
Chris D Brown <owner@chrisdbrown.co.uk>
2019-02-16 18:09:31 +0000

    fix: Resolve mobile usability issues

--- src/App.vue
+++ src/App.vue
@@ -69,6 +69,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+@media only screen and (max-width: 600px) {
+  section .el-main {
+    padding: 20px 0 20px 0;
+  }
 }
 </style>
--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,50 +1,48 @@
 <template>
   <div>
-    <table>
-      <div v-if="location.type == 'Default'">
-        <td>Acquired by default when BLU is unlocked</td>
-      </div>
+    <li v-if="location.type == 'Default'">
+      Acquired by default when BLU is unlocked
+    </li>
 
-      <div v-if="location.type == 'Dungeon'">
-        <td>{{ location.enemy }} (level {{ location.level }}) in the Dungeon "{{ location.name }}"</td>
-      </div>
+    <li v-if="location.type == 'Dungeon'">
+      {{ location.enemy }} (level {{ location.level }}) in the Dungeon "{{ location.name }}"
+    </li>
 
-      <div v-if="location.type == 'FATE'">
-        <td>{{ location.enemy }} (level {{ location.level }}) in the FATE "{{ location.name }}" in {{ location.area }} (x{{ location.coordinates.x }},y{{ location.coordinates.y }}) <el-button icon="el-icon-search" circle @click="showMap(location)"></el-button></td>
-      </div>
+    <li v-if="location.type == 'FATE'">
+      {{ location.enemy }} (level {{ location.level }}) in the FATE "{{ location.name }}" in {{ location.area }} (x{{ location.coordinates.x }},y{{ location.coordinates.y }}) <el-button icon="el-icon-search" circle @click="showMap(location)"></el-button>
+    </li>
 
-      <div v-if="location.type == 'Hunt Mark'">
-        <td>Hunt Mark {{ location.enemy }} (level {{ location.level }}) in {{ location.area }}</td>
-      </div>
+    <li v-if="location.type == 'Hunt Mark'">
+      Hunt Mark {{ location.enemy }} (level {{ location.level }}) in {{ location.area }}
+    </li>
 
-      <div v-if="location.type == 'Levequest'">
-        <td>{{ location.enemy }} (level {{ location.level }}) in the Levequest "{{ location.name }}" in {{ location.area }} (x{{ location.coordinates.x }},y{{ location.coordinates.y }}) <el-button icon="el-icon-search" circle @click="showMap(location)"></el-button></td>
-      </div>
+    <li v-if="location.type == 'Levequest'">
+      {{ location.enemy }} (level {{ location.level }}) in the Levequest "{{ location.name }}" in {{ location.area }} (x{{ location.coordinates.x }},y{{ location.coordinates.y }}) <el-button icon="el-icon-search" circle @click="showMap(location)"></el-button>
+    </li>
 
-      <div v-if="location.type == 'Masked Carnivale Enemy'">
-        <td>{{ location.enemy }} in Masked Carnivale stage {{ location.stage }}, "{{ location.name }}"</td>
-      </div>
+    <li v-if="location.type == 'Masked Carnivale Enemy'">
+      {{ location.enemy }} in Masked Carnivale stage {{ location.stage }}, "{{ location.name }}"
+    </li>
 
-      <div v-if="location.type == 'Masked Carnivale Stages'">
-        <td>Clear stage {{ location.stage }} of the Masked Carnivale</td>
-      </div>
+    <li v-if="location.type == 'Masked Carnivale Stages'">
+      Clear stage {{ location.stage }} of the Masked Carnivale
+    </li>
 
-      <div v-if="location.type == 'Open World'">
-        <td>{{ location.enemy }} (level {{ location.level }} in {{ location.area }} (x{{ location.coordinates.x }},y{{ location.coordinates.y }}) <el-button icon="el-icon-search" circle @click="showMap(location)"></el-button></td>
-      </div>
+    <li v-if="location.type == 'Open World'">
+      {{ location.enemy }} (level {{ location.level }} in {{ location.area }} (x{{ location.coordinates.x }},y{{ location.coordinates.y }}) <el-button icon="el-icon-search" circle @click="showMap(location)"></el-button>
+    </li>
 
-      <div v-if="location.type == 'Raid'">
-        <td>{{ location.enemy }} (level {{ location.level }}) in the Raid "{{ location.name }}"</td>
-      </div>
+    <li v-if="location.type == 'Raid'">
+      {{ location.enemy }} (level {{ location.level }}) in the Raid "{{ location.name }}"
+    </li>
 
-      <div v-if="location.type == 'Spells Learned'">
-        <td>Learn {{ location.number }} spells, then speak to the Mamool-Ja in Ul'Dah next to the BLU class trainer</td>
-      </div>
+    <li v-if="location.type == 'Spells Learned'">
+      Learn {{ location.number }} spells, then speak to the Mamool-Ja in Ul'Dah next to the BLU class trainer
+    </li>
 
-      <div v-if="location.type == 'Trial'">
-        <td>{{ location.enemy }} (level {{ location.level }}) in the Trial "{{ location.name }}"</td>
-      </div>
-    </table>
+    <li v-if="location.type == 'Trial'">
+      {{ location.enemy }} (level {{ location.level }}) in the Trial "{{ location.name }}"
+    </li>
   </div>
 </template>
 
@@ -60,3 +58,10 @@
     }
   }
 </script>
+
+<style>
+  .location-list .el-button.is-circle {
+    padding: 8px;
+    margin-left: 5px;
+  }
+</style>
