4276c3d842aec22ea5fab833bee21b7aaef0f17d
Chris D Brown <owner@chrisdbrown.co.uk>
2019-02-22 17:30:16 +0000

    feat: Generate and use screen-size appropriate map images

---
+++ helpers/responsivemaps.js
@@ -0,0 +1,15 @@
+// @TODO: Move this whole process to webpack rather than doing this
+const fs = require('fs')
+const sharp = require('sharp')
+const sizes = require('../src/assets/sizes.json')
+
+const inputFolder = 'helpers/maps/'
+const outputFolder = 'public/maps/'
+
+fs.readdirSync(inputFolder).forEach(file => {
+  sizes.forEach(size => {
+    sharp(inputFolder + file)
+      .resize(size.generatedWidth)
+      .toFile(outputFolder + file.split('.').slice(0, -1).join('.') + '-' + size.generatedWidth + 'w.jpg')
+  })
+})
